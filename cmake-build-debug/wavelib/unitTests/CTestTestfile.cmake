a6600112faa21df0542b805c394a3680f466288a
slplsswkds <slplsswkds@protonmail.com>
2024-06-01 20:56:05 +0300

    Initial project version

---
+++ cmake-build-debug/wavelib/unitTests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/maks/devel/bw/wavelib/unitTests
+# Build directory: /home/maks/devel/bw/cmake-build-debug/wavelib/unitTests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("wavelibBoostTests")
