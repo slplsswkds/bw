4466dc35517d930a3e074b7c983349d28652c5c2
slplsswkds <slplsswkds@protonmail.com>
2024-06-12 08:52:59 +0300

    print frequency for decomposition levels

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -24,7 +24,7 @@ auto load_data() {
         double number;
         while (dataFile >> number) {
             numbers.push_back(number);
-            std::println("{}", number);
+            // std::println("{}", number);
         }
         dataFile.close();
     } else {
@@ -33,9 +33,27 @@ auto load_data() {
     return numbers;
 }
 
+std::tuple<double, double, double> freq_detail_min_max(int j, int fs) {
+    double minF = 0;
+    double midF = fs / std::pow(2, j + 1);
+    double maxF = fs / std::pow(2, j);
+    return std::make_tuple(minF, midF, maxF);
+}
+
+void freq(int j, int fs) {
+    for (int i = 3; i <= j; i++) {
+        double min, mid, max;
+        std::tie(min, mid, max) = freq_detail_min_max(i, fs);
+        std::println("for J={}: \t{}Hz\t-\t{}Hz\t-\t{}Hz", i, min, mid, max);
+    }
+}
+
 int main() {
-    const int N = 10240;
+    const int N = 44100;
+    const int sampleRate = 44100; // Hz
     const int J = 10;
+
+    freq(J, sampleRate);
 
     const auto inp = new double[N];
     const auto out = new double[N];
@@ -50,24 +68,19 @@ int main() {
     setDWTExtension(wt, "sym"); // Options are "per" and "sym". Symmetric is the default option
     setWTConv(wt, "direct");
     dwt(wt, inp); // Perform DWT
-    // DWT output can be accessed using wt->output vector. Use wt_summary to find out how to extract appx and detail coefficients
-
-    for (int i = 0; i < wt->outlength; ++i) {
-        printf("%g ", wt->output[i]);
-    }
 
     idwt(wt, out); // Perform IDWT (if needed)
+
     // Test Reconstruction
     for (int i = 0; i < wt->siglength; ++i) {
         diff[i] = out[i] - inp[i];
     }
-
     printf("\nMAX %g\n", absmax(diff, wt->siglength)); // If Reconstruction succeeded then the output should be a small value.
 
-    wt_summary(wt); // Prints the full summary.
+    // wt_summary(wt); // Prints the full summary.
+
     wave_free(obj);
     wt_free(wt);
-
     delete[] inp;
     delete[] out;
     delete[] diff;
