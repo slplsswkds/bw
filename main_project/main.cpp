20c73d38ff9e7ce5e5ea70b19e78d1942dcd97f2
slplsswkds <slplsswkds@protonmail.com>
2024-06-14 02:36:43 +0300

    NeighShrink (not tested)

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -1,12 +1,52 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include "../wavelib/header/wavelib.h"
 #include <algorithm>
 #include <fstream>
 #include <print>
 #include <vector>
 #include <iostream>
+#include <array>
+#include "../wavelib/header/wavelib.h"
+
+int compare_double(const void *a, const void *b) {
+    double arg1 = *(const double *) a;
+    double arg2 = *(const double *) b;
+
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
+double median(double *x, int N) {
+    double sigma;
+
+    qsort(x, N, sizeof(double), compare_double);
+
+    if ((N % 2) == 0) {
+        sigma = (x[N / 2 - 1] + x[N / 2]) / 2.0;
+    } else {
+        sigma = x[N / 2];
+    }
+
+    return sigma;
+}
+
+// Function to calculate the mean square of an array
+double mean_square(const double *array, int size) {
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i] * array[i];
+    }
+    return sum / size;
+}
+
+// Function to calculate SNR
+double snr(const int size, const double *signal, const double *noise) {
+    const double signal_power = mean_square(signal, size);
+    const double noise_power = mean_square(noise, size);
+    return 10.0 * log10(signal_power / noise_power);
+}
 
 double absmax(const double *array, const int N) {
     double max = 0.0;
@@ -78,19 +118,19 @@ static auto rmse(const int N, const double *x, const double *y) -> double {
     return rms;
 }
 
-auto rezeroConcretLevelTest(const wt_object wt, const int J) {
+auto rezeroConcretLevelTest(const wt_object wt) {
     int offset = 0;
     std::cout << wt->outlength;
     for (int wtOutputVectorElement = 0; wtOutputVectorElement <= wt->MaxIter; wtOutputVectorElement++) {
         const auto decompositionLvlSize = wt->length[wtOutputVectorElement];
 
-        if (wtOutputVectorElement == J) {
+        if (wtOutputVectorElement == wt->J) {
             std::cout << "------ LEVEL SIZE = " << decompositionLvlSize << std::endl;
             std::cout << "------ Iteration № " << wtOutputVectorElement << "-------" << std::endl;
 
             for (int coefPos = 0; coefPos < decompositionLvlSize; coefPos++) {
                 const auto coef = wt->output[offset + coefPos];
-                wt->output[offset + coefPos] = 0;
+                // wt->output[offset + coefPos] = 0;
             }
 
             offset += decompositionLvlSize;
@@ -98,10 +138,60 @@ auto rezeroConcretLevelTest(const wt_object wt, const int J) {
     }
 }
 
+void neighshrink(const wt_object wt) {
+    int offset = 0;
+    for (int j = 0; j <= wt->J; j++) {
+        if (j != 0) {
+            // skip if it is approximation coefficienst
+            const auto lvlSize = wt->length[j];
+
+            // split signal into windows and apply neighshrink
+            int windows = 10; // number of windows
+            int windowSize = ceil(lvlSize / windows);
+
+            for (int window = 0; window < windows; window++) {
+                for (int coefPos = 0; coefPos < windowSize; coefPos++) {
+                    double *pCoefAddr = &wt->output[window * windowSize + coefPos];
+
+                    // copy window to calculate median
+                    const auto pWindowCopy = new double[windowSize];
+                    std::copy_n(&wt->output[offset], windowSize, pWindowCopy);
+                    delete[] pWindowCopy;
+
+                    double sigma = sigma = median(pWindowCopy, windowSize) / 0.6745;
+                    double lambda = sigma * sqrt(2 * log(windowSize)) / (1 + log(j)); // or log(wt->J-j)
+
+                    const bool coefIsFirs = (coefPos == 0);
+                    const bool coefIsLast = (coefPos == (windowSize - 1));
+
+                    double nPow2;
+                    if (coefPos == 0 || coefIsLast) {
+                        const double coef = *pCoefAddr;
+                        nPow2 = std::pow(coef, 2);
+                    } else {
+                        double prevCoef = *(pCoefAddr - 1);
+                        double coef = *pCoefAddr;
+                        double nextCoef = *(pCoefAddr + 1);
+                        nPow2 = std::pow(prevCoef, 2) + std::pow(coef, 2) + std::pow(nextCoef, 2);
+                    }
+
+                    std::println("sigma = {}, nPow2 = {}, lambda = {}", sigma, nPow2, lambda);
+                    if (nPow2 >= lambda) {
+                        // wt->output[offset + coefPos] = wt->output[offset + coefPos] * (1 - std::pow(lambda, 2)/nPow2);
+                    } else {
+                        *pCoefAddr = 0;
+                    }
+                }
+            }
+            offset += lvlSize;
+        }
+    }
+}
+
 int main() {
     constexpr int N = 44101;
     constexpr int sampleRate = 44100; // Hz
-    constexpr int J = 11;
+    constexpr int J = 12;
 
     freq(J, sampleRate);
 
@@ -118,9 +208,10 @@ int main() {
     setDWTExtension(wt, "sym"); // Options are "per" and "sym". Symmetric is the default option
     setWTConv(wt, "direct");
     dwt(wt, inp); // Perform DWT
-    rezeroConcretLevelTest(wt, J);
+    // rezeroConcretLevelTest(wt);
+    // neighshrink(wt);
 
-    idwt(wt, out); // Perform IDWT (if needed)
+    idwt(wt, out); // Perform IDWT
 
     // Test Reconstruction
     for (int i = 0; i < wt->siglength; ++i) {
@@ -131,10 +222,10 @@ int main() {
     auto rmseVal = rmse(N, inp, out);
     std::println("\nRMSE(inp, out) = {}\n", rmseVal);
 
+    auto snrVal = snr(N, inp, out);
+    std::println("\nsnr(inp, out) = {}dB\n", snrVal);
+
     // wt_summary(wt); // Prints the full summary.
-    // for (int i = 0; i < wt->outlength; ++i) {
-    //     printf("%g ",wt->output[i]);
-    // }
 
     wave_free(obj);
     wt_free(wt);
